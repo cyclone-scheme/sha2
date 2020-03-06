253c9392faf4f997792ecb89e363951149525d35
Justin Ethier <justin.ethier@gmail.com>
2020-03-06 14:53:49 -0500

    Added usage message

--- sha256sum.scm
+++ sha256sum.scm
@@ -1,5 +1,10 @@
 (import (scheme base) (scheme write) (cyclone crypto sha2))
 
+(when (null? (command-line-arguments))
+  (display "Usage: sha256sum FILE")
+  (newline)
+  (exit 1))
+
 (display
   (sha-256 (open-binary-input-file (car (command-line-arguments)))))
 (display "  ")
